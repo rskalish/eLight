9b1af61deb9f0941787f6a759c2e7fcf547222a3
rskalish <rskalish@lohika.com>
2016-03-10 16:46:14 +0200

    added missed files

---
+++ Components/ResourceKeeper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace eLight.Components
+{
+    public sealed class ResourceKeeper
+    {
+        public Image FlashOnImg = new Image
+        {
+            Source = new BitmapImage(new Uri("ms-appx:///Assets/light_bulb_on.png", UriKind.Absolute))
+        };
+
+        public Image FlashOffImg = new Image
+        {
+            Source = new BitmapImage(new Uri("ms-appx:///Assets/light_bulb_off.png", UriKind.Absolute))
+        };
+
+        private static volatile ResourceKeeper _instance;
+        private static readonly object SyncRoot = new Object();
+
+        private ResourceKeeper() { }
+
+        public static ResourceKeeper Instance
+        {
+           get 
+           {
+              if (_instance == null) 
+              {
+                 lock (SyncRoot) 
+                 {
+                    if (_instance == null)
+                        _instance = new ResourceKeeper();
+                 }
+              }
+
+              return _instance;
+           }
+        }
+    }
+}
---
+++ Components/ScreenLight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace eLight.Components
+{
+    public class ScreenLight
+    {
+        public async void HiglightSreen()
+        {
+            Grid contentGrid = new Grid
+            {
+                Height = Window.Current.Bounds.Height,
+                Width = Window.Current.Bounds.Width
+            };
+
+            ContentDialog dlg = new ContentDialog { Content = contentGrid };
+            SolidColorBrush color = new SolidColorBrush(Colors.White) { Opacity = 1 };
+            dlg.Background = color;
+            await dlg.ShowAsync();
+        }
+    }
+}
